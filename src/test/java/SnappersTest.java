82a4939b8c9e65dfc69d085598a4fa581099dcf1
Michael Nitschinger <michael@nitschinger.at>
2019-08-25 22:19:11 +0200

    Bump to new version

--- src/test/java/SnappersTest.java
+++ src/test/java/SnappersTest.java
@@ -1,7 +1,6 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.nio.charset.Charset;
 
